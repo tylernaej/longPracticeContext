5064e7a637723925ba45f96e61cb522ac5689471
An Bui <dobui0115@gmail.com>
2022-06-02 14:40:46 -0700

    solved 6-8

--- practice-for-week-04-context-long-practice-main/problems/07-all-the-args.js
+++ practice-for-week-04-context-long-practice-main/problems/07-all-the-args.js
@@ -1,7 +1,11 @@
-function allTheArgs(func, ...args) {
-  // Your code here
+function allTheArgs(func, ...args) {//rest to accept multiple args: [arg1],[arg2],[arg3]
+  console.log(args);
+  console.log(func);
+  return func.bind(null, ...args);//spread to comma seperated args
 }
-
+//we need to use  bind because we don't want to invoke the func immediately
+//APPLY can only takes in 2 args(CONTEXT,ARRAY)
+//BIND take in any numbers of args
 /*****************************************************************************/
 /***************** DO NOT MODIFY ANYTHING UNDER THIS LINE ********************/
 
--- practice-for-week-04-context-long-practice-main/problems/08-call-me-later.js
+++ practice-for-week-04-context-long-practice-main/problems/08-call-me-later.js
@@ -1,11 +1,38 @@
-// Your code here
+/*
+Create a CallCenter class.
+The constructor function should take in a name argument and set the name property of a newly instantiated instance to the name argument.
+Add two instance methods: a. sayHello() - prints Hello this is ${name} b. callMeLater(delay) - takes in a delay in milliseconds that will call the sayHello() method on the instance after the delay. The context of the sayHello() method when invoked needs to be the CallCenter instance that the method was called on.
+You CANNOT use bind, call, or apply for this phase!
 
+Test your implementation by running the test specs in the test/08-call-me-later-spec.js file. Run the specs with the following command:
+
+npm test test/08-call-me-later-spec.js
+*/
+
+class CallCenter {
+  constructor(name) {
+    this.name = name;
+  }
+
+  sayHello() {
+    //console.log(this);
+    console.log(`Hello this is ${this.name}`);
+  }
+
+  callMeLater(delay) {
+	  //we need to use anonymous func for setTimeout to keep the Callcenter context
+	  //otherwise, the context is the Timeout obj
+    setTimeout(() => {
+      this.sayHello();
+    }, delay);
+  }
+}
 
 /*****************************************************************************/
 /***************** DO NOT MODIFY ANYTHING UNDER THIS LINE ********************/
 
 try {
-	module.exports = CallCenter;
+  module.exports = CallCenter;
 } catch {
-	module.exports = null;
+  module.exports = null;
 }
